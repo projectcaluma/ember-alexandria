7bf9c89f6b1226e664d4aa2fdb65613bef1952e1
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-07-23 15:10:56 +0200

    feat(nav): add basic category nav and tagging

---
+++ addon/models/tag.js
@@ -0,0 +1,9 @@
+import Model, { attr, hasMany } from "@ember-data/model";
+
+export default class TagModel extends Model {
+  @attr name;
+  @attr description;
+
+  @hasMany documents;
+
+}
---
+++ config/icons.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+  return {
+    "free-solid-svg-icons": ["folder", "ellipsis-v"],
+    "free-regular-svg-icons": ["file-alt"],
+  };
+};
