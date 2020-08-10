81952e076034c36285d1478fd46e9db74d6ae01e
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-08-10 08:00:41 +0200

    feat(search): add basic search to document view

--- addon/controllers/application.js
+++ addon/controllers/application.js
@@ -2,8 +2,9 @@ import Controller from "@ember/controller";
 import { tracked } from "@glimmer/tracking";
 
 export default class ApplicationController extends Controller {
-  queryParams = ["category", "tags"];
+  queryParams = ["category", "tags", "search"];
   @tracked category;
-  @tracked tags;
   // Cant use @tracked tags = []; because of https://github.com/emberjs/ember.js/issues/19078
+  @tracked tags;
+  @tracked search;
 }
