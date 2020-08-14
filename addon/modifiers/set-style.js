d658afd6818d585ef37586c5b3a5e18e03e38217
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-08-14 17:51:29 +0200

    fix(set-style): pass style through htmlSafe

--- addon/modifiers/set-style.js
+++ addon/modifiers/set-style.js
@@ -1,7 +1,8 @@
+import { htmlSafe } from "@ember/template";
 import { modifier } from "ember-modifier";
 
 export default modifier(function setStyle(element, _, styles) {
   Object.entries(styles).forEach(([key, value]) => {
-    element.style[key] = value;
+    element.style[key] = htmlSafe(value);
   });
 });
