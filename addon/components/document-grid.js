dd8bc22c88423cc6097d8ff7f90e6bf1f60fd705
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2020-11-17 21:16:26 +0100

    feat(document-grid): add drag'n'drop capability to document grid
    This duplicates quite a few lines of code. But I didn't see a way
    to reuse the code from the upload button component. Maybe we should
    move those actions to a service that can be used by all components
    where we could also put the upload and delete actions.
    But this is - IMO - out of scope for this PR. I will gladly make
    another PR with the service.

--- addon/components/document-grid.js
+++ addon/components/document-grid.js
@@ -1,11 +1,17 @@
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import { wrapGrid } from "animate-css-grid";
 import { lastValue, task } from "ember-concurrency-decorators";
 
 export default class DocumentGridComponent extends Component {
+  @service notification;
+  @service config;
   @service store;
+  @service intl;
+
+  @tracked isDragOver = false;
 
   get selectedDocument() {
     if (this.args.selectedDocumentId) {
@@ -42,5 +48,84 @@ export default class DocumentGridComponent extends Component {
 
   @action setupGridAnimations(element) {
     wrapGrid(element);
+  }
+
+  // Drag'n'Drop document upload
+
+  @action onDragEnter() {
+    if (!this.args.filters.category) {
+      return;
+    }
+
+    this.isDragOver = true;
+  }
+
+  @action onDragLeave() {
+    this.isDragOver = false;
+  }
+
+  @action onDragOver(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  @action async onDrop(event) {
+    if (!this.args.filters.category) {
+      return;
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    const { files } = event.dataTransfer;
+
+    try {
+      const category =
+        (await this.store.peekRecord("category", this.args.filters.category)) ||
+        this.store.findRecord("category", this.args.filters.category);
+
+      await Promise.all(
+        Array.from(files).map(async (file) => {
+          const documentModel = this.store.createRecord("document", {
+            category,
+            meta: this.config.defaultModelMeta.document,
+          });
+          documentModel.title = file.name;
+          await documentModel.save();
+
+          const fileModel = this.store.createRecord("file", {
+            name: file.name,
+            type: "original",
+            document: documentModel,
+          });
+          await fileModel.save();
+
+          const response = await fetch(fileModel.uploadUrl, {
+            method: "PUT",
+            body: file,
+          });
+
+          if (!response.ok) {
+            throw new Error("The request returned an error status code");
+          }
+        })
+      );
+
+      this.notification.success(
+        this.intl.t("alexandria.success.upload-document", {
+          count: files.length,
+        })
+      );
+
+      await this.fetchDocuments.perform();
+    } catch (error) {
+      this.notification.danger(
+        this.intl.t("alexandria.errors.upload-document", {
+          count: files.length,
+        })
+      );
+    }
+
+    this.isDragOver = false;
   }
 }
