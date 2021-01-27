a59b13179cda47bb3c17e6138def336e2e31c98a
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2021-01-27 10:17:46 +0100

    fix(routes): add empty model hook and param options to route
    Without the empty model hook the route throws if activeRoute is set.
    And without the additional param configuration those won't work.

--- addon/routes/application.js
+++ addon/routes/application.js
@@ -1,10 +1,20 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
 
+const PARAM_OPTIONS = { refreshModel: true };
+
 export default class ApplicationRoute extends Route {
-  queryParams = { activeGroup: { refreshModel: true } };
+  queryParams = {
+    category: PARAM_OPTIONS,
+    tags: PARAM_OPTIONS,
+    search: PARAM_OPTIONS,
+    document: PARAM_OPTIONS,
+    activeGroup: PARAM_OPTIONS,
+  };
 
   @service config;
+
+  model() {}
 
   afterModel(model, transition) {
     this.config.alexandriaQueryParams = transition.to.parent.params;
