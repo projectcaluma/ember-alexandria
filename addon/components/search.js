042d24db3659f81421b964e820f0658709a4b3fb
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2021-01-27 12:42:00 +0100

    fix(search): add preventDefault to submit
    Enter inside a text-input submits the form which is a problem.

--- addon/components/search.js
+++ addon/components/search.js
@@ -1,3 +1,4 @@
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { timeout } from "ember-concurrency";
@@ -5,6 +6,10 @@ import { restartableTask } from "ember-concurrency-decorators";
 
 export default class SearchComponent extends Component {
   @service router;
+
+  @action onSubmit(event) {
+    event.preventDefault();
+  }
 
   @restartableTask *updateSearch({ target: { value: search } }) {
     if (!Ember.testing) {
