3810f05e0c6a334e5f2b48ea1524ccdde5f90495
derrabauke <pfralf@googlemail.com>
2021-07-15 15:18:14 +0200

    feat: sorting for document-list

--- tests/dummy/mirage/factories/document.js
+++ tests/dummy/mirage/factories/document.js
@@ -7,7 +7,7 @@ export default Factory.extend({
   title: () => setAllLocales(faker.commerce.product()),
   description: () => setAllLocales(faker.company.catchPhrase()),
   createdByUser: "dummy",
-  createdByGroup: "dummy-group",
+  createdByGroup: () => setAllLocales(faker.company.companyName()),
   createdAt: () => faker.date.past(),
   thumbnail: null,
 
