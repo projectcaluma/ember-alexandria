78f0bb0cafc9e2c8438beec974fd1672cf4dd9d5
derrabauke <pfralf@googlemail.com>
2021-07-15 13:39:40 +0200

    Cleanup after auto-generated code - delete component import autside of addon directory - add engine resolver to test files

--- tests/integration/components/document-grid-test.js
+++ tests/integration/components/document-grid-test.js
@@ -2,10 +2,14 @@ import { render } from "@ember/test-helpers";
 import setupRenderingTest from "dummy/tests/helpers/setup-rendering-test";
 import { hbs } from "ember-cli-htmlbars";
 import { setupMirage } from "ember-cli-mirage/test-support";
+import engineResolverFor from "ember-engines/test-support/engine-resolver-for";
 import { module, todo } from "qunit";
 
+const modulePrefix = "ember-alexandria";
+const resolver = engineResolverFor(modulePrefix);
+
 module("Integration | Component | document-grid", function (hooks) {
-  setupRenderingTest(hooks);
+  setupRenderingTest(hooks, { resolver });
   setupMirage(hooks);
 
   todo("it renders a document grid", async function (assert) {
--- tests/integration/components/document-view-toggle-test.js
+++ tests/integration/components/document-view-toggle-test.js
@@ -1,10 +1,14 @@
 import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
+import engineResolverFor from "ember-engines/test-support/engine-resolver-for";
 import { setupRenderingTest } from "ember-qunit";
 import { module, test } from "qunit";
 
+const modulePrefix = "ember-alexandria";
+const resolver = engineResolverFor(modulePrefix);
+
 module("Integration | Component | document-view-toggle", function (hooks) {
-  setupRenderingTest(hooks);
+  setupRenderingTest(hooks, { resolver });
 
   test("it renders", async function (assert) {
     this.set("viewList", true);
