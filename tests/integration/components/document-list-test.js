c4e2e27054ab096141989c5342513c837b64d00b
Stephan Hug <stephan.hug@adfinis.com>
2021-08-10 15:10:33 +0200

    added tests for the document list item

--- tests/integration/components/document-list-test.js
+++ tests/integration/components/document-list-test.js
@@ -1,5 +1,5 @@
-// import { render } from "@ember/test-helpers";
-// import { hbs } from "ember-cli-htmlbars";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
 import { setupMirage } from "ember-cli-mirage/test-support";
 import engineResolverFor from "ember-engines/test-support/engine-resolver-for";
 import { setupIntl } from "ember-intl/test-support";
@@ -14,18 +14,36 @@ module("Integration | Component | document-list", function (hooks) {
   setupIntl(hooks, "en");
   setupMirage(hooks);
 
-  // test("it renders the supplied list of documents with the correct title", async function (assert) {
-  //   const createdDocs = this.server.createList("document", 3);
-  //   this.set("fetchedDocuments", createdDocs);
-  //   // await render(hbs`<DocumentList />`);
-  //   // assert.equal(this.element.textContent.trim(), "");
-  //   // Template block usage:
-  //   await render(hbs`
-  //     <DocumentList/>
-  //   `);
-  //   assert.equal(this.element.textContent.trim(), createdDocs[0]);
-  // });
+  // @loading={{this.fetchDocuments}}
+  // @selectedDocumentId={{this.selectedDocumentId}}
+  // @fetchedDocuments={{this.fetchedDocuments}}
+  // @setSort={{this.setSort}}
+  // @selectedDocuments={{this.selectedDocuments}}
+  // @onClickDocument={{this.handleDocumentSelection}}
 
-  test("it renders the supplied list of documents with the correct title");
-  test("clicking the sorting buttons enables the correct sorting function");
+  test("it renders the supplied list of documents with the correct title", async function (assert) {
+    // const createdDocs = this.server.createList("document", 3);
+    // console.log("🦠 createdDocs:", createdDocs);
+    this.fetchedDocuments = [
+      { title: "document1" },
+      { title: "document2" },
+      { title: "document3" },
+    ];
+
+    //   this.set("fetchedDocuments", createdDocs);
+    //   // await render(hbs`<DocumentList />`);
+    //   // assert.equal(this.element.textContent.trim(), "");
+    //   // Template block usage:
+    await render(hbs`
+      <DocumentList
+        @loading=false
+        @selectedDocuments=[]
+        @fetchedDocuments={{this.fetchedDocuments}}
+        @setSort=""
+      />
+    `);
+    assert.equal(this.element.textContent.trim(), "document1");
+  });
+
+  // test("clicking the sorting buttons enables the correct sorting function");
 });
