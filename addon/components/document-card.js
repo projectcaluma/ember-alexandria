6f88a4ec5a3255da868b4b9e0a9843822af3f18c
Stephan Hug <stephan.hug@adfinis.com>
2021-08-27 14:33:55 +0200

    feat: preparing for zip download implementation

--- addon/components/document-card.js
+++ addon/components/document-card.js
@@ -12,12 +12,10 @@ export default class DocumentCardComponent extends Component {
   }
 
   @task *download() {
-    if (this.args.document) {
-      // TODO: Currently this only works for a single document
+    if (this.args.document || this.args?.documents.length === 1) {
+      const doc = this.args.document || this.args?.documents[0];
       try {
-        const file = this.args.document.files.find(
-          (file) => file.type === "original"
-        );
+        const file = doc.files.find((file) => file.type === "original");
         const extension = file.name.includes(".")
           ? `.${file.name.split(".").slice(-1)[0]}`
           : "";
@@ -25,10 +23,24 @@ export default class DocumentCardComponent extends Component {
         // There is a known issue with file-saver and urls.
         // The filename passed as the second argument is ignored.
         // https://github.com/eligrey/FileSaver.js/issues/670
-        yield saveAs(file.downloadUrl, this.args.document.title + extension);
+        yield saveAs(file.downloadUrl, doc.title + extension);
       } catch (error) {
         this.notification.danger(this.intl.t("alexandria.errors.save-file"));
       }
+    } else {
+      // Compile an array of original file PKs
+      const originalFilePKs = encodeURIComponent(
+        this.args.documents
+          .map((doc) =>
+            doc.files.toArray().find((file) => file.type === "original")
+          )
+          .map((f) => f.id)
+          .join(",")
+      );
+
+      // const url = `/api/v1/files/multi?filter%5Bfiles%5D=${originalFileIds}`;
+      const url = `api/v1/files/multi?files=${originalFilePKs}`;
+      yield saveAs(url, `Download-${this.args.documents.length}-files.zip`);
     }
   }
 }
