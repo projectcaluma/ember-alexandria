8305da07ce86f3cd970cef070c8a5069fd08a1d5
Stephan Hug <stephan.hug@adfinis.com>
2021-09-10 14:37:22 +0200

    fix: change ember-lts to 3.28 and CI should still run

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,10 +7,10 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: "ember-lts-3.24",
+        name: "ember-lts-3.28",
         npm: {
           devDependencies: {
-            "ember-source": "~3.24.0",
+            "ember-source": "~3.28.0",
           },
         },
       },
