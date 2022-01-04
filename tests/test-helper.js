1df788f55a67750001eefd60cc62fbad66efd601
Falk <falk.neumann@adfinis.com>
2022-01-04 17:15:06 +0100

    chore!(deps):  upgrade core deps
    BREAKING CHANGE:
    - minimum node version: 12.22 and above or node >15

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,13 @@
+import * as QUnit from "qunit";
+import { setup } from "qunit-dom";
 import { setApplication } from "@ember/test-helpers";
 import { start } from "ember-qunit";
 import setupSinon from "ember-sinon-qunit";
 
 import Application from "../app";
 import config from "../config/environment";
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
