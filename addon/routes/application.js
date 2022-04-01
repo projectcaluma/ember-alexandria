ed8a93273c6e300f5c5c93b9b1663a38387681f1
Stephan Hug <stephan.hug@adfinis.com>
2022-04-01 13:36:39 +0200

    fix: reset document selection

--- addon/routes/application.js
+++ addon/routes/application.js
@@ -1,6 +1,5 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
-import { isEmpty } from "@ember/utils";
 
 const PARAM_OPTIONS = { refreshModel: true };
 
@@ -14,13 +13,14 @@ export default class ApplicationRoute extends Route {
   };
 
   @service config;
+  @service documents;
 
   model() {}
 
   afterModel(model, transition) {
     /* Depending on how ember alexandria is used we might have to reset
     the document selection on navigation */
-    if (isEmpty(transition.to.queryParams)) {
+    if (Object.keys(transition.to.queryParams).length === 0) {
       this.documents.clearDocumentSelection();
     }
     this.config.alexandriaQueryParams = transition.to.parent.params;
