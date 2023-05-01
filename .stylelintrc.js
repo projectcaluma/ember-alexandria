4d029a80d77199dadb646664b841a8dae5945e66
Jonas Metzener <jonas.metzener@adfinis.com>
2023-05-01 15:19:44 +0200

    chore(lint): fix css linting errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,4 +2,8 @@
 
 module.exports = {
   extends: ["stylelint-config-standard-scss", "stylelint-prettier/recommended"],
+  rules: {
+    "selector-class-pattern": null,
+    "scss/at-extend-no-missing-placeholder": null,
+  },
 };
