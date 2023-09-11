35901b93b0dfb39a0464dd94a62057d8d382d37b
Christian Zosel <christian@zosel.ch>
2023-09-11 10:07:38 +0200

    fix(download): don't add extension if already present
    and make comment on filename attribute more precise

--- addon/components/document-card.js
+++ addon/components/document-card.js
@@ -21,12 +21,14 @@ export default class DocumentCardComponent extends Component {
         const extension = file.name.includes(".")
           ? `.${file.name.split(".").slice(-1)[0]}`
           : "";
+        const fileName = doc.title.endsWith(extension)
+          ? doc.title
+          : doc.title + extension;
 
-        // There is a known issue with file-saver and urls.
-        // The filename passed as the second argument is ignored.
-        // https://github.com/eligrey/FileSaver.js/issues/670
-
-        yield saveAs(file.downloadUrl, doc.title + extension);
+        // keep in mind that for the filename to be considered, the request needs
+        // to be same-origin (i.e. object storage needs to be deployed with a proxy)
+        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a?retiredLocale=de#attributes
+        yield saveAs(file.downloadUrl, fileName);
       } catch {
         this.notification.danger(this.intl.t("alexandria.errors.save-file"));
       }
