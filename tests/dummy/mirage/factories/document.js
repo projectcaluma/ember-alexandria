c061196d338647fcec9195a0a996fc5c17ce2716
Jonas Metzener <jonas.metzener@adfinis.com>
2023-10-31 10:15:30 +0100

    feat(document): add manual document date

--- tests/dummy/mirage/factories/document.js
+++ tests/dummy/mirage/factories/document.js
@@ -6,10 +6,12 @@ import { setAllLocales } from "./helpers";
 export default Factory.extend({
   title: () => setAllLocales(faker.commerce.product()),
   description: () => setAllLocales(faker.company.catchPhrase()),
-  createdByUser: "dummy",
+  createdByUser: () => faker.person.fullName(),
   createdByGroup: () => faker.company.name(),
   createdAt: () => faker.date.past(),
   thumbnail: null,
+  modifiedAt: () => faker.date.past(),
+  date: () => (Math.random() >= 0.5 ? faker.date.past() : null),
 
   afterCreate(document, server) {
     document.update({
