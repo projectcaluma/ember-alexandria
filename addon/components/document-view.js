7b766283eab60e9cfbc21ff3e1ae9e8f575b8459
Jonas Metzener <jonas.metzener@adfinis.com>
2023-11-20 18:12:27 +0100

    fix(upload): show loading spinner on drag & drop upload

--- addon/components/document-view.js
+++ addon/components/document-view.js
@@ -2,7 +2,7 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { lastValue, task } from "ember-concurrency";
+import { dropTask, lastValue, task } from "ember-concurrency";
 
 import { ErrorHandler } from "ember-alexandria/helpers/error-handler";
 
@@ -96,7 +96,7 @@ export default class DocumentViewComponent extends Component {
     event.stopPropagation();
   }
 
-  @action async onDrop(event) {
+  onDrop = dropTask(async (event) => {
     if (!this.args.filters.category) {
       return;
     }
@@ -126,7 +126,7 @@ export default class DocumentViewComponent extends Component {
     }
 
     this.isDragOver = false;
-  }
+  });
 
   // * DOCUMENT SELECTION
   handleKeyDown(event) {
