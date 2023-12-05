c4e730e7f99ac693b99a77ea76c83cd87faa3586
Jonas Metzener <jonas.metzener@adfinis.com>
2023-12-05 13:00:20 +0100

    feat(document): allow moving of a document per drag & drop

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -9,21 +9,10 @@ export default function makeServer(config) {
       this.namespace = "/api/v1";
       this.timing = 400;
 
-      this.get("/categories");
-      this.get("/categories/:id");
-
-      this.get("/documents");
-      this.get("/documents/:id");
-      this.patch("/documents/:id");
-      this.post("/documents");
-      this.delete("/documents/:id");
-
-      this.get("/tags");
-      this.get("/tags/:id");
-      this.post("/tags");
-      this.patch("/tags/:id");
-
-      this.get("/marks");
+      this.resource("categories", { only: ["index", "show"] });
+      this.resource("documents");
+      this.resource("tags", { except: ["delete"] });
+      this.resource("marks", { only: ["index"] });
 
       this.post("/files", function (schema) {
         const attrs = this.normalizedRequestAttrs();
--- tests/integration/components/document-list-item-test.js
+++ tests/integration/components/document-list-item-test.js
@@ -1,8 +1,9 @@
-import { click, doubleClick, render } from "@ember/test-helpers";
+import { click, doubleClick, render, triggerEvent } from "@ember/test-helpers";
 import { setupRenderingTest } from "dummy/tests/helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { setupMirage } from "ember-cli-mirage/test-support";
 import { module, test } from "qunit";
+import { fake } from "sinon";
 
 module("Integration | Component | document-list-item", function (hooks) {
   setupRenderingTest(hooks);
@@ -16,8 +17,9 @@ module("Integration | Component | document-list-item", function (hooks) {
       marks: [],
     };
     this.isSelected = false;
-    this.onClickDocument = () => {};
-    this.onDoubleClickDocument = () => {};
+    this.onClickDocument = fake();
+    this.onDoubleClickDocument = fake();
+    this.onDragStart = fake();
 
     await render(hbs`
       <DocumentListItem
@@ -26,6 +28,7 @@ module("Integration | Component | document-list-item", function (hooks) {
         @isSelected={{this.isSelected}}
         @onClickDocument={{this.onClickDocument}}
         @onDoubleClickDocument={{this.onDoubleClickDocument}}
+        @onDragStart={{this.onDragStart}}
       />
     `);
   });
@@ -37,18 +40,23 @@ module("Integration | Component | document-list-item", function (hooks) {
   });
 
   test("it fires the onClickDocument function with the correct parameter", async function (assert) {
-    this.set("onClickDocument", (arg) => {
-      assert.strictEqual(arg, this.document);
-    });
-
     await click("[data-test-document-list-item]");
+
+    assert.strictEqual(this.onClickDocument.callCount, 1);
+    assert.deepEqual(this.onClickDocument.args[0][0], this.document);
   });
 
   test("it fires the onDoubleClickDocument function with the correct parameter", async function (assert) {
-    this.set("onDoubleClickDocument", (arg) => {
-      assert.strictEqual(arg, this.document);
-    });
-
     await doubleClick("[data-test-document-list-item]");
+
+    assert.strictEqual(this.onDoubleClickDocument.callCount, 1);
+    assert.deepEqual(this.onDoubleClickDocument.args[0][0], this.document);
+  });
+
+  test("it fires the onDragStart function with the correct parameter", async function (assert) {
+    await triggerEvent("[data-test-document-list-item]", "dragstart");
+
+    assert.strictEqual(this.onDragStart.callCount, 1);
+    assert.deepEqual(this.onDragStart.args[0][0], this.document);
   });
 });
