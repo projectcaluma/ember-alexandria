projectcaluma/ember-alexandria

commit 8261bbb3e283c9b8001e45dd200bc2cec0a67b65

refactor: tag and mark filter with no request

addon/components/document-view.js
@@ -2,7 +2,8 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { dropTask, lastValue, task } from "ember-concurrency";
+import { dropTask, task } from "ember-concurrency";
+import { task as trackedTask } from "ember-resources/util/ember-concurrency";
 
 import { ErrorHandler } from "ember-alexandria/helpers/error-handler";
 
@@ -19,6 +20,8 @@ export default class DocumentViewComponent extends Component {
   @tracked listView = true;
   @tracked sort = "title";
   @tracked sortDirection = "";
+  // Needed for ember-resource
+  @tracked uploadedDocuments = 0;
 
   constructor(parent, args) {
     super(parent, args);
@@ -47,10 +50,15 @@ export default class DocumentViewComponent extends Component {
     this.router.transitionTo(this.router.currentRouteName, {
       queryParams: { sort: this.sortDirection + this.sort },
     });
-    this.fetchDocuments.perform();
   }
 
-  @lastValue("fetchDocuments") fetchedDocuments;
+  fetchedDocuments = trackedTask(this, this.fetchDocuments, () => [
+    this.sort,
+    this.sortDirection,
+    this.args.filters,
+    this.uploadedDocuments,
+  ]);
+
   @task
   *fetchDocuments() {
     const documents = yield this.store.query("document", {
@@ -114,8 +122,7 @@ export default class DocumentViewComponent extends Component {
           count: files.length,
         }),
       );
-
-      await this.fetchDocuments.perform();
+      this.afterUpload();
     } catch (error) {
       new ErrorHandler(this, error).notify(
         "alexandria.errors.upload-document",
@@ -135,7 +142,7 @@ export default class DocumentViewComponent extends Component {
     }
     if (event.key === "a" && event.ctrlKey) {
       event.preventDefault();
-      this.fetchedDocuments.forEach((doc) => {
+      this.fetchedDocuments.value.forEach((doc) => {
         this.documents.selectDocument(doc);
       });
     }
@@ -163,8 +170,9 @@ export default class DocumentViewComponent extends Component {
 
     // SHIFT SELECTION
     if (event.shiftKey) {
-      const selectedDocIndex = this.fetchedDocuments.indexOf(selectedDocument);
-      const firstSelectedDocIndex = this.fetchedDocuments.indexOf(
+      const selectedDocIndex =
+        this.fetchedDocuments.value.indexOf(selectedDocument);
+      const firstSelectedDocIndex = this.fetchedDocuments.value.indexOf(
         this.documents.selectedDocuments[0],
       );
 
@@ -194,5 +202,10 @@ export default class DocumentViewComponent extends Component {
       (file) => file.variant === "original",
     );
     open(file.downloadUrl);
+  }
+
+  @action
+  afterUpload() {
+    this.uploadedDocuments++;
   }
 }

addon/components/document-grid.hbs
@@ -4,7 +4,7 @@
   ...attributes
   {{did-insert this.setupGridAnimations}}
 >
-  {{#if this.fetchDocuments.isRunning}}
+  {{#if @loading}}
     {{#each (range 0 10) as |index|}}
       <div>
         <DocumentCard::Skeleton @animationDelay={{concat index "00ms"}} />

addon/components/document-view.hbs
@@ -8,12 +8,13 @@
     <div class="uk-flex uk-flex-middle uk-padding-small">
       <DocumentUploadButton
         @category={{@filters.category}}
-        @afterUpload={{perform this.fetchDocuments}}
+        @afterUpload={{this.afterUpload}}
         data-test-upload
       />
 
       <TagFilter
         @category={{@filters.category}}
+        @documents={{this.fetchedDocuments.value}}
         @selectedTags={{@filters.tags}}
         @selectedMarks={{@filters.marks}}
         class="uk-margin-left"
@@ -29,11 +30,10 @@
 
     {{! Drag'n'Drop Area }}
     <div
-      class="
-      uk-position-relative uk-height-1-1
-      {{if this.isDragOver "document-grid--dragover"}}
-       {{unless this.canDrop "document-grid--disallowed"}}
-      "
+      class="uk-position-relative uk-height-1-1
+        {{if this.isDragOver 'document-grid--dragover'}}
+        {{unless this.canDrop 'document-grid--disallowed'}}
+        "
       {{on "dragenter" this.onDragEnter}}
       {{on "dragleave" this.onDragLeave}}
       {{on "dragover" this.onDragOver}}
@@ -45,15 +45,13 @@
       >
         <div
           class="document-view"
-          {{did-insert (perform this.fetchDocuments)}}
-          {{did-update (perform this.fetchDocuments) @filters}}
           {{did-insert (perform this.initialiseDocumentSelection)}}
         >
           {{! List & Grid View }}
           {{#if this.listView}}
             <DocumentList
-              @loading={{this.fetchDocuments}}
-              @fetchedDocuments={{this.fetchedDocuments}}
+              @loading={{this.fetchedDocuments.isRunning}}
+              @fetchedDocuments={{this.fetchedDocuments.value}}
               @setSort={{this.setSort}}
               @selectedDocuments={{this.documents.selectedDocuments}}
               @onClickDocument={{this.handleDocumentSelection}}
@@ -61,9 +59,9 @@
             />
           {{else}}
             <DocumentGrid
-              @loading={{this.fetchDocuments.isRunning}}
+              @loading={{this.fetchedDocuments.isRunning}}
+              @fetchedDocuments={{this.fetchedDocuments.value}}
               @selectedDocuments={{this.documents.selectedDocuments}}
-              @fetchedDocuments={{this.fetchedDocuments}}
               @onClickDocument={{this.handleDocumentSelection}}
               @onDoubleClickDocument={{this.openDocument}}
             />

addon/components/tag-filter.hbs
@@ -2,23 +2,27 @@
   class="uk-border uk-padding-small uk-width-expand uk-flex uk-flex-wrap uk-flex-middle"
   ...attributes
 >
-  {{#each this.activeMarks.value as |mark|}}
-    <button
-      type="button"
-      class="tag uk-text-small
-        {{if (includes mark.id this.parsedSelected.marks) 'tag--active'}}"
-      {{on "click" (fn this.toggle "marks" mark)}}
-      data-test-mark
-      data-test-mark-id={{mark.id}}
-      title={{mark.name}}
-    >
-      <FaIcon @icon={{mark.icon}} @size="sm" @fixedWidth={{true}} />
-    </button>
-  {{/each}}
-  {{#if (and this.searchTags.isFinished (not this.searchTags.value))}}
-    <span class="uk-margin-small-left">{{t "alexandria.tag-filter.empty"}}</span>
+  {{#if this.marks.marks.hasRan}}
+    {{#each this.availableMarks.value as |mark|}}
+      <button
+        type="button"
+        class="tag uk-text-small
+          {{if (includes mark.id this.parsedSelected.marks) 'tag--active'}}"
+        {{on "click" (fn this.toggle "marks" mark)}}
+        data-test-mark
+        data-test-mark-id={{mark.id}}
+        title={{mark.name}}
+      >
+        <FaIcon @icon={{mark.icon}} @size="sm" @fixedWidth={{true}} />
+      </button>
+    {{/each}}
+  {{/if}}
+  {{#if (and this.availableTags.isFinished (not this.availableTags.value))}}
+    <span class="uk-margin-small-left">{{t
+        "alexandria.tag-filter.empty"
+      }}</span>
   {{else}}
-    {{#each this.tags.fetchSearchTags.lastSuccessful.value as |tag|}}
+    {{#each this.availableTags.value as |tag|}}
       <button
         type="button"
         class="tag uk-text-small

addon/components/tag-filter.js
@@ -17,28 +17,39 @@ export default class TagFilterComponent extends Component {
     };
   }
 
-  searchTags = trackedFunction(this, async () => {
-    return this.tags.fetchSearchTags.perform(this.args.category);
+  availableTags = trackedFunction(this, async () => {
+    if (!this.args.documents) {
+      return [];
+    }
+
+    const availableTags = await this.args.documents.reduce(async (acc, doc) => {
+      acc = await acc;
+      (await doc.tags).forEach((tag) => acc.add(tag.id));
+
+      return acc;
+    }, new Set());
+
+    return this.store.peekAll("tag").filter((tag) => availableTags.has(tag.id));
   });
 
-  activeMarks = trackedFunction(this, async () => {
-    const activeMarks = await this.store
-      .peekAll("document")
-      .reduce(async (acc, doc) => {
-        const marks = await doc.marks;
+  availableMarks = trackedFunction(this, async () => {
+    if (!this.args.documents) {
+      return [];
+    }
+
+    const availableMarks = await this.args.documents.reduce(
+      async (acc, doc) => {
         acc = await acc;
-        if (
-          (this.args.category === undefined ||
-            (await doc.category) === this.args.category) &&
-          marks.length > 0
-        ) {
-          marks.forEach((mark) => acc.add(mark.id));
-        }
+        (await doc.marks).forEach((mark) => acc.add(mark.id));
 
         return acc;
-      }, new Set());
+      },
+      new Set(),
+    );
 
-    return this.marks.marks.records?.filter((mark) => activeMarks.has(mark.id));
+    return this.marks.marks.records?.filter((mark) =>
+      availableMarks.has(mark.id),
+    );
   });
 
   @action toggle(type, value) {

addon/services/tags.js
@@ -2,7 +2,6 @@ import { action } from "@ember/object";
 import Service, { inject as service } from "@ember/service";
 import { dasherize } from "@ember/string";
 import { tracked } from "@glimmer/tracking";
-import { task } from "ember-concurrency";
 
 import { ErrorHandler } from "ember-alexandria/helpers/error-handler";
 
@@ -11,19 +10,6 @@ export default class TagsService extends Service {
   @service config;
 
   @tracked categoryCache;
-
-  @task *fetchSearchTags(category) {
-    this.categoryCache = category;
-
-    return yield this.store.query("tag", {
-      filter: {
-        withDocumentsInCategory: this.categoryCache,
-        withDocumentsMetainfo: JSON.stringify(
-          this.config.modelMetaFilters.document,
-        ),
-      },
-    });
-  }
 
   /**
    * Adds a tag to a document and creates the tag if necessary. Returns the added tag
@@ -69,8 +55,6 @@ export default class TagsService extends Service {
       new ErrorHandler(this, error).notify("alexandria.errors.update");
     }
 
-    await this.fetchSearchTags.perform(this.categoryCache);
-
     return tag;
   }
 
@@ -95,7 +79,5 @@ export default class TagsService extends Service {
       document.tags = tags;
       new ErrorHandler(this, error).notify("alexandria.errors.update");
     }
-
-    this.fetchSearchTags.perform(this.categoryCache);
   }
 }

tests/acceptance/documents-test.js
@@ -260,9 +260,13 @@ module("Acceptance | documents", function (hooks) {
   });
 
   test("changing the category clears the tag selection", async function (assert) {
-    await this.server.createList("document", 3);
+    const documents = await this.server.createList("document", 3);
     const tag = await this.server.create("tag");
     const category = await this.server.create("category");
+
+    documents[0].update({
+      tags: [tag],
+    });
 
     await visit("/");
 
@@ -302,6 +306,10 @@ module("Acceptance | documents", function (hooks) {
   test("selecting a document does not clear the tag selection", async function (assert) {
     const documents = await this.server.createList("document", 2);
     const tag = await this.server.create("tag");
+
+    documents[0].update({
+      tags: [tag],
+    });
 
     await visit("/");
     await click(`[data-test-tag-id="${tag.id}"]`);

tests/integration/components/document-view-test.js
@@ -71,8 +71,8 @@ module("Integration | Component | document-view", function (hooks) {
 
     await render(hbs`<DocumentView @filters={{this.filters}} />`);
 
-    assert.strictEqual(requests.length, 4, "store handled 4 requests");
-    assert.deepEqual(requests[1].queryParams, {
+    assert.strictEqual(requests.length, 3, "store handled 3 requests");
+    assert.deepEqual(requests[0].queryParams, {
       "filter[title]": "test",
       "filter[description]": "bla",
       include: "category,files,tags",

commit f8707da21abd110a25dab772f81ffa572c39b838

fix(selection): fix regression in shift selection
Fixes regression introduced in 8261bbb3e283c9b8001e45dd200bc2cec0a67b65

addon/components/document-view.js
@@ -195,7 +195,7 @@ export default class DocumentViewComponent extends Component {
 
       this.documents.clearDocumentSelection();
       for (let i = startIndex; i <= endIndex; i++) {
-        this.documents.selectDocument(this.fetchedDocuments[i]);
+        this.documents.selectDocument(this.fetchedDocuments.value[i]);
       }
     }
   }