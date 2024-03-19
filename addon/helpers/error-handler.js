d5983d8a83cb077c70db58b9e78cfe3754d2a31b
Jonas Metzener <jonas.metzener@adfinis.com>
2024-03-19 12:25:32 +0100

    fix(errors): display root level validation messages from backend

--- addon/helpers/error-handler.js
+++ addon/helpers/error-handler.js
@@ -12,10 +12,20 @@ export class ErrorHandler {
 
   notify(...args) {
     console.error(this.error);
-    if (this.error?.errors?.[0].status === "403") {
+
+    const firstError = this.error?.errors?.[0];
+
+    if (firstError?.status === "403") {
       return this.notification.danger(
         this.intl.t("alexandria.errors.no-permission"),
       );
+    } else if (
+      firstError?.status === "400" &&
+      firstError?.source.pointer === "/data"
+    ) {
+      // Root level validation errors should be displayed directly from the
+      // backend to allow proper custom validation
+      return this.notification.danger(firstError.detail);
     }
 
     return this.notification.danger(this.intl.t(...args));
