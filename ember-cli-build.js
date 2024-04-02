d49ce85719dad0b7d379f9276296beb678081aa0
yelinz <yelin.zhang@adfinis.com>
2024-04-02 11:43:30 +0200

    chore: remove old config from ember-cli-build
    add pnpm ember try to gitignore

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,11 +3,7 @@
 const EmberAddon = require("ember-cli/lib/broccoli/ember-addon");
 
 module.exports = function (defaults) {
-  const app = new EmberAddon(defaults, {
-    "ember-simple-auth": {
-      useSessionSetupMethod: true,
-    },
-  });
+  const app = new EmberAddon(defaults, {});
 
   /*
     This build file specifies the options for the dummy test app of this
