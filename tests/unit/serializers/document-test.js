4be4c0ca140ae411221a57386364f32daf8e948e
yelinz <yelin.zhang@adfinis.com>
2024-04-29 17:26:32 +0200

    fix: remove content attr in serialization for patch

--- tests/unit/serializers/document-test.js
+++ tests/unit/serializers/document-test.js
@@ -4,19 +4,34 @@ import { module, test } from "qunit";
 module("Unit | Serializer | document", function (hooks) {
   setupTest(hooks);
 
-  test("it exists", function (assert) {
-    const store = this.owner.lookup("service:store");
-    const serializer = store.serializerFor("document");
-
-    assert.ok(serializer);
-  });
-
   test("it serializes records", function (assert) {
     const store = this.owner.lookup("service:store");
-    const record = store.createRecord("document", {});
+    const record = store.createRecord("document", {
+      title: { en: "Foo" },
+      content: "test",
+    });
 
     const serializedRecord = record.serialize();
 
-    assert.ok(serializedRecord);
+    // content attribute should be removed
+    assert.deepEqual(serializedRecord, {
+      data: {
+        attributes: {
+          "created-at": undefined,
+          "created-by-group": undefined,
+          "created-by-user": undefined,
+          date: undefined,
+          description: {},
+          metainfo: undefined,
+          "modified-at": undefined,
+          "modified-by-group": undefined,
+          "modified-by-user": undefined,
+          title: {
+            en: "Foo",
+          },
+        },
+        type: "documents",
+      },
+    });
   });
 });
