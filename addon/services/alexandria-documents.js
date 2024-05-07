527d2683710ceaaa4878e85a262ebb25b61c841f
Christian Zosel <christian@zosel.ch>
2024-05-07 17:00:33 +0200

    chore: simplify mime type check

--- addon/services/alexandria-documents.js
+++ addon/services/alexandria-documents.js
@@ -64,7 +64,7 @@ export default class AlexandriaDocumentsService extends Service {
       if (
         category.allowedMimeTypes &&
         !category.allowedMimeTypes.includes(
-          file.type || mime.getType(file.name.split(".").pop()),
+          file.type || mime.getType(file.name),
         )
       ) {
         return this.notification.danger(
