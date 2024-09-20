3017e81cfcbad933f736c49f73537215c6e19a90
yelinz <yelin.zhang@adfinis.com>
2024-09-20 21:12:51 +0200

    fix(nav): upload of preview images

--- tests/integration/components/category-nav/category-test.js
+++ tests/integration/components/category-nav/category-test.js
@@ -55,7 +55,7 @@ module("Integration | Component | category-nav/category", function (hooks) {
     await triggerEvent("[data-test-drop]", "drop", {
       dataTransfer: {
         getData: () => documents.map((d) => d.id).join(","),
-        files: [],
+        files: [new File(["Thumbnail"], "test-file.txt")],
       },
     });
 
@@ -91,7 +91,7 @@ module("Integration | Component | category-nav/category", function (hooks) {
 
     await triggerEvent("[data-test-drop]", "drop", {
       dataTransfer: {
-        getData: () => {},
+        getData: () => "",
         files: [new File(["Ember Rules!"], "test-file.txt")],
       },
     });
