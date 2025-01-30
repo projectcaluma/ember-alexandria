bee477612cb8d846dc652f95eaabfe585aab5231
Niels Verbeek <niels.verbeek@adfinis.com>
2025-01-30 16:25:58 +0100

    feat(copy): drag-drop ctrl modifier to copy a document

--- tests/integration/components/document-view-test.js
+++ tests/integration/components/document-view-test.js
@@ -1,4 +1,4 @@
-import { render } from "@ember/test-helpers";
+import { render, triggerEvent } from "@ember/test-helpers";
 import { setupRenderingTest } from "dummy/tests/helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { setupMirage } from "ember-cli-mirage/test-support";
@@ -56,5 +56,41 @@ module("Integration | Component | document-view", function (hooks) {
       include: "category,files,tags",
       sort: "title",
     });
+  });
+
+  test("copy documents within category", async function (assert) {
+    const category = this.server.create("category", {
+      allowedMimeTypes: ["image/png"],
+    });
+    const documents = this.server.createList("document", 3, {
+      categoryId: category.id,
+      title: "test.png",
+    });
+
+    const docService = this.engine.lookup("service:alexandria-documents");
+    this.filters = { categories: category.id };
+
+    const docSelection = documents.slice(0, 2);
+    docService.selectedDocuments = docSelection;
+
+    await render(
+      hbs`
+          <DocumentView @listView={{ false }} @filters={{ this.filters }} />`,
+      {
+        owner: this.engine,
+      },
+    );
+
+    assert.dom("[data-test-empty]").doesNotExist();
+    assert.dom("[data-test-document]").exists({ count: 3 });
+
+    await triggerEvent("[data-drop-documents]", "drop", {
+      ctrlKey: true,
+      dataTransfer: {
+        getData: () => docSelection.map((d) => d.id).join(","),
+      },
+    });
+
+    assert.dom("[data-test-document]").exists({ count: 5 });
   });
 });
