57c4c782b4ad5e2fbcb2343041414fcfe2722f92
Yelin Zhang <30687616+Yelinz@users.noreply.github.com>
2025-02-11 11:53:52 +0100

    fix: update packages (#1069)

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,6 @@
 import { setApplication } from "@ember/test-helpers";
-import { start } from "ember-qunit";
+import { start, setupEmberOnerrorValidation } from "ember-qunit";
+import { loadTests } from "ember-qunit/test-loader";
 import setupSinon from "ember-sinon-qunit";
 import * as QUnit from "qunit";
 import { setup } from "qunit-dom";
@@ -13,4 +14,6 @@ setApplication(Application.create(config.APP));
 
 setupSinon();
 
+setupEmberOnerrorValidation();
+loadTests();
 start();
