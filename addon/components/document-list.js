45cf8d1692361afad072baddb2de014b49779460
Stephan Hug <stephan.hug@adfinis.com>
2025-07-01 16:23:08 +0200

    feat: add sorting icon that changes depending on current sort param

--- addon/components/document-list.js
+++ addon/components/document-list.js
@@ -1,7 +1,19 @@
+import { action } from "@ember/object";
 import Component from "@glimmer/component";
 
 export default class DocumentListComponent extends Component {
   get columnCount() {
     return Object.keys(this.args.columns).length;
+  }
+
+  @action
+  sortIcon(sortKey, currentSort) {
+    if (currentSort === sortKey) {
+      return "sort-up";
+    }
+    if (currentSort === `-${sortKey}`) {
+      return "sort-down";
+    }
+    return "sort";
   }
 }
