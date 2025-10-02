365d465e4f85eb779c0a01a41046b6f44a6ac929
Tilo Stoppe <tilo.stoppe@adfinis.com>
2025-10-02 16:26:30 +0200

    feat: add document count display and provide category filtering logic

--- addon/components/document-view.js
+++ addon/components/document-view.js
@@ -4,6 +4,7 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { task } from "ember-concurrency";
 import { task as trackedTask } from "reactiveweb/ember-concurrency";
+import { trackedFunction } from "reactiveweb/function";
 
 export default class DocumentViewComponent extends Component {
   @service notification;
@@ -90,10 +91,27 @@ export default class DocumentViewComponent extends Component {
   ]);
 
   get documentsInCategory() {
-    console.log("category IDs", this.fetchedDocuments.value.map(doc => doc.category.id));
-    console.log("selected category", this.args.category)
-    return this.fetchedDocuments.value.filter(doc => doc.category.id == this.args.category);
+    return this.args.category
+      ? this.fetchedDocuments.value?.filter((doc) =>
+          this.categoryIds?.value?.includes(doc.category.get("id")),
+        )
+      : this.fetchedDocuments.value;
   }
+
+  categoryIds = trackedFunction(this, () => {
+    if (this.args.category) {
+      const cat = this.store.peekRecord("category", this.args.category);
+
+      const categoryIds = [cat.get("id")];
+      if (cat.children) {
+        const childIds = cat.children.map((category) => category.get("id"));
+        categoryIds.push(...childIds);
+      }
+      return categoryIds;
+    }
+
+    return [];
+  });
 
   @task
   *fetchDocuments() {
