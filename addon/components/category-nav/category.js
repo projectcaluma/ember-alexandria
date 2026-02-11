6323b1f856cd8277609406961b7337bfa5da652d
Niels Verbeek <niels.verbeek@adfinis.com>
2026-02-11 15:29:23 +0100

    feat(document): add feature flag to enable copy fallback on move

--- addon/components/category-nav/category.js
+++ addon/components/category-nav/category.js
@@ -8,6 +8,7 @@ import { confirm } from "ember-uikit";
 import { trackedFunction } from "reactiveweb/function";
 
 export default class CategoryNavCategoryComponent extends Component {
+  @service("alexandria-config") config;
   @service("alexandria-documents") documents;
   @service notification;
   @service router;
@@ -136,11 +137,27 @@ export default class CategoryNavCategoryComponent extends Component {
 
     if (failed.length > 0) {
       const permissionDenied = failed.filter((state) => state.error === 403);
-      if (permissionDenied.length > 0) {
+      if (permissionDenied.length > 0 && this.config.enableMoveCopyFallback) {
         await this.copyFailedMoves(permissionDenied, this.args.category);
       }
 
-      if (failed.length > permissionDenied.length) {
+      if (!this.config.enableMoveCopyFallback) {
+        if (permissionDenied.length > 0) {
+          this.notification.danger(
+            this.intl.t("alexandria.errors.move-failed", {
+              count: failed.length,
+              documentTitle: failed[0].document.title,
+              categoryName: this.args.category.name,
+            }),
+          );
+        } else {
+          this.notification.danger(
+            this.intl.t("alexandria.errors.move-document", {
+              count: failed.length,
+            }),
+          );
+        }
+      } else if (failed.length > permissionDenied.length) {
         this.notification.danger(
           this.intl.t("alexandria.errors.move-document", {
             count: failed.length - permissionDenied.length,
@@ -170,7 +187,7 @@ export default class CategoryNavCategoryComponent extends Component {
 
   async copyFailedMoves(documentInfos, newCategory) {
     const copyConfirmed = await confirm(
-      this.intl.t("alexandria.errors.move-failed", {
+      this.intl.t("alexandria.errors.move-failed-fallback", {
         count: documentInfos.length,
         documentTitle: documentInfos[0].document.title,
         categoryName: newCategory.name,
